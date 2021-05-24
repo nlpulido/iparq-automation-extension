21f021414bd10c5d8d0a6a21f969ec69746cffee
Neil Pulido <neilpphoto@gmail.com>
2021-05-24 11:23:34 -0700

    changed the extension to be an NPM Project

---
+++ src/popup.js
@@ -0,0 +1,38 @@
+'use strict';
+
+import './popup.css';
+
+window.onload = (function() {
+  function changeStatusMessage(message) {
+    document.getElementById('status').innerHTML() = message;
+  }
+
+  function setUpExtension() {
+
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+      console.log(request);
+    });
+
+    document.getElementById('validateBtn').addEventListener('click', () => {
+      // edit the status
+      changeStatusMessage("SENDING MESSAGE...");
+
+      // Communicate with background file by sending a message
+      chrome.runtime.sendMessage(
+        {
+          type: 'GREETINGS',
+          payload: {
+            message: 'Hello, my name is Pop. I am from Popup.',
+          },
+        },
+        response => {
+          console.log(response.message);
+        }
+      );
+    });
+
+  }
+
+  // on load, set up our listeners for the elements
+  document.addEventListener('DOMContentLoaded', setUpExtension);
+})();
