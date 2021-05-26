3ceab1e0ae0990950ab63d28e5fe251aca440740
Neil Pulido <neilpphoto@gmail.com>
2021-05-26 11:27:52 -0700

    added conditional rendering to the extension

--- src/popup.js
+++ src/popup.js
@@ -10,6 +10,16 @@ window.onload = (function() {
   }
 
   function setUpExtension() {
+    // check if we're on an iPARQ URL
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+
+      // rewrite popup.html if we're not on an admin iPARQ site
+      if (!tabs[0].url.includes("admin.thepermitstore")) {
+        const inner = document.getElementById('inner');
+        inner.innerHTML = "<h1>Please use the extension on the iPARQ admin site.</h1>";
+      }
+
+    });
 
     // add a listener for when the user clicks start
     document.getElementById('validateBtn').addEventListener('click', () => {
